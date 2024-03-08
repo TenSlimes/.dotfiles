d682094e15a206bfe6a2a0012ba5280d4276e8a7
TenSlimes <alitpurnama13@gmail.com>
2024-03-09 03:14:33 +0900

    chores(kernels): blacklist kernel
    - blacklist kernel based on nvidia settings on flake.nix

--- modules/kernels/default.nix
+++ modules/kernels/default.nix
@@ -1,40 +1,45 @@
-{ config, lib, pkgs, modulesPath, ... }:
-let
-  vfioIds = [ "10de:2188" "10de:1aeb" ];
-in  {
+{ systemSettings, config, lib, pkgs, modulesPath, ... }:
+let nvidiaSettings = systemSettings.gpu.nvidia;
+in {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [
-      "kvm-amd"
-      #"vfio_virqfd"
-      #"vfio_pci"
-      #"vfio_iommu_type1"
-      #"vfio"
-      "nvidia"
-      "nvidia_modeset"
-      "nvidia_uvm"
-      "nvidia_drm"
-    ];
-    kernelParams = [
-      "amd_iommu=on"
-      "amd_iommu=pt"
-      "kvm.ignore_mrs=1"
-      #"nvidia_drm.modeset=1"
-    ];
-    blacklistedKernelModules= [
-      "nouveau"
-     # "nvidia"
-     # "nvidia_modeset"
-     # "nvidia_uvm"
-     # "nvidia_drm"          
-    ];
-    # options nouveau.config=NvGspRm=1
-    #extraModprobeConfig = ''
-    #'';
+    kernelModules = [ "kvm-amd" ]
+      ++ (if (nvidiaSettings.enable && nvidiaSettings.open == true) then
+        [
+
+        ]
+      else
+        (if (nvidiaSettings.enable && nvidiaSettings.open == false) then [
+          "nvidia"
+          "nvidia_modeset"
+          "nvidia_uvm"
+          "nvidia_drm"
+        ] else
+          [ ]));
+    kernelParams = [ "amd_iommu=on" "amd_iommu=pt" "kvm.ignore_mrs=1" ]
+      ++ (if (nvidiaSettings.enable && nvidiaSettings.open == false) then
+        ([ "nvidia_drm.modeset=1" ])
+      else
+        ([ ]));
+    blacklistedKernelModules =
+      if (nvidiaSettings.open && nvidiaSettings.enable) then [
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_uvm"
+        "nvidia_drm"
+      ] else
+        (if (nvidiaSettings.enable && nvidiaSettings.open == false) then
+          [ "nouveau" ]
+        else
+          [ ]);
+    extraModprobeConfig =
+      if (nvidiaSettings.open && nvidiaSettings.enable) then ''
+        options nouveau.config=NvGspRm=1
+      '' else
+        "";
     supportedFilesystems = [ "ntfs" ];
   };
 
-  
-# VM Shit
-  users.users.alit.extraGroups = ["qemu-libvirtd" "libvirtd" "disk" "kvm"];
+  # VM Shit
+  users.users.alit.extraGroups = [ "qemu-libvirtd" "libvirtd" "disk" "kvm" ];
 }
