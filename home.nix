c0e714b49c15a435dae201b57e66b1c544bed26a
TenSlimes <alitpurnama13@gmail.com>
2024-03-18 05:11:26 +0900

    refactor(home.nix): add support for conditional colorscheme

--- home.nix
+++ home.nix
@@ -2,20 +2,27 @@
 let
 
   nix-colors-lib = inputs.nix-colors.lib.contrib { inherit pkgs; };
+
+  userTheme = userSettings.theme;
 in {
   imports = [
     ./modules
     inputs.nix-index-database.hmModules.nix-index
     inputs.nix-colors.homeManagerModules.default
+    inputs.sops-nix.homeManagerModules.sops
   ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
 
   #colorScheme = inputs.nix-colors.colorSchemes.${userSettings.colors.slug};
-  colorScheme = nix-colors-lib.colorSchemeFromPicture {
-    path = ./modules/hypr/config/wallpapers/sky-butterfly.jpg;
-    variant = "dark";
-  };
+  colorScheme = if (userTheme.colorScheme == userTheme.wallpaperPath) then
+    nix-colors-lib.colorSchemeFromPicture {
+      path = userTheme.wallpaperPath;
+      variant = userTheme.variant;
+    }
+  else
+    inputs.nix-colors.colorSchemes.${userTheme.colorScheme};
 
   home.username = "alit";
   home.homeDirectory = "/home/alit";
@@ -27,74 +34,18 @@ in {
       name = "mochaDark";
     };
   };
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
     (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "FiraCode" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
   ];
-
-  #dconf.settings = {
-  #  "org/virt-manager/virt-manager/connections" = {
-  #    autoconnect = ["qemu:///system"];
-  #    uris = ["qemu:///system"];
-  #  };
-  #};
 
   xdg = { enable = true; };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-    #"~/.config/hypr/hyprland.conf".source = ./config/hypr/hyprland.conf;
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/alit/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = { EDITOR = "nvim"; };
 
-  # Let Home Manager install and manage itself.
-  programs = {
-    home-manager.enable = true;
-    fish = { enable = true; };
-  };
+  programs = { home-manager.enable = true; };
 
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "steam" "steam-original" "steam-run" ];
